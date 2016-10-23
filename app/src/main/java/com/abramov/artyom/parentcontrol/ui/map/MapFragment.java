dd789a7883f6f57b4e574b6b73e17bd075f34167
w3ggy <strike4321@gmail.com>
2016-10-23 16:12:17 +0400

    Added welcome screen. Changed paalette

--- app/src/main/java/com/abramov/artyom/parentcontrol/ui/map/MapFragment.java
+++ app/src/main/java/com/abramov/artyom/parentcontrol/ui/map/MapFragment.java
@@ -81,11 +81,10 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
                 Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
                 ActivityCompat.checkSelfPermission(getActivity(),
                         Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            mMap.setMyLocationEnabled(true);
-            mMap.getUiSettings().setMyLocationButtonEnabled(true);
+            map.setMyLocationEnabled(true);
+            map.getUiSettings().setMyLocationButtonEnabled(true);
+            map.getUiSettings().setRotateGesturesEnabled(true);
         }
-
-
     }
 
     private void startLocationService() {
--- app/src/main/java/com/abramov/artyom/parentcontrol/ui/welcome_screen/WelcomeActivity.java
+++ app/src/main/java/com/abramov/artyom/parentcontrol/ui/welcome_screen/WelcomeActivity.java
@@ -1,19 +1,72 @@
 package com.abramov.artyom.parentcontrol.ui.welcome_screen;
 
+import android.Manifest;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
-/**
- * Created by Artyom on 09.09.2016.
- */
+import com.abramov.artyom.parentcontrol.R;
+import com.abramov.artyom.parentcontrol.ui.main_screen.MainActivity;
 
-public class WelcomeActivity extends AppCompatActivity {
+import agency.tango.materialintroscreen.MaterialIntroActivity;
+import agency.tango.materialintroscreen.MessageButtonBehaviour;
+import agency.tango.materialintroscreen.SlideFragmentBuilder;
+
+public class WelcomeActivity extends MaterialIntroActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        addSlide(new SlideFragmentBuilder()
+                .backgroundColor(R.color.colorPrimary)
+                .buttonsColor(R.color.colorAccent)
+                .possiblePermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION})
+                .neededPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION})
+                .image(agency.tango.materialintroscreen.R.drawable.ic_next)
+                .title(getString(R.string.welcome_map_title))
+                .description(getString(R.string.welcome_map_description))
+                .build());
 
+        addSlide(new SlideFragmentBuilder()
+                .backgroundColor(R.color.colorPrimary)
+                .buttonsColor(R.color.colorAccent)
+                .possiblePermissions(new String[]{Manifest.permission.READ_PHONE_STATE})
+                .neededPermissions(new String[]{Manifest.permission.READ_PHONE_STATE})
+                .image(agency.tango.materialintroscreen.R.drawable.ic_next)
+                .title(getString(R.string.welcome_phone_title))
+                .description(getString(R.string.welcome_phone_description))
+                .build());
+
+        addSlide(new SlideFragmentBuilder()
+                .backgroundColor(R.color.colorPrimary)
+                .buttonsColor(R.color.colorAccent)
+                .possiblePermissions(new String[]{Manifest.permission.READ_SMS})
+                .neededPermissions(new String[]{Manifest.permission.READ_SMS})
+                .image(agency.tango.materialintroscreen.R.drawable.ic_next)
+                .title(getString(R.string.welcome_sms_title))
+                .description(getString(R.string.welcome_sms_description))
+                .build());
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    private void changeScreen() {
+        startActivity(new Intent(this, MainActivity.class));
+    }
+
+    @Override
+    public void onFinish() {
+        changeScreen();
     }
 }
